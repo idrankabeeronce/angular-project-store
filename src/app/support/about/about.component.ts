84f6426c802d2985a1ae35fd5ccaa0e2eba8b3ca
huyerve <mrhuk06@gmail.com>
2022-12-04 17:59:59 +0400

    added video in about page

--- src/app/support/about/about.component.ts
+++ src/app/support/about/about.component.ts
@@ -13,7 +13,9 @@ export class AboutComponent implements OnInit {
   textExample_1:any = [{title: "About Us", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam gravida porttitor sapien sed pellentesque. Cras non turpis nec dui venenatis pellentesque quis in neque. Vivamus suscipit sed nisi ac placerat. Nulla vestibulum metus mi, ac efficitur urna cursus id. Phasellus malesuada at nulla non feugiat. Nulla aliquet magna non eleifend volutpat. Ut dapibus justo eget velit aliquet, id rhoncus elit dignissim. Quisque quis ullamcorper dui, tempus accumsan nisi. Aenean venenatis ultrices augue vitae venenatis. Quisque dictum lectus eu lorem molestie, ut sodales dui tincidunt."},{title: "What we do", text: "Suspendisse pretium neque a urna maximus aliquam. Proin pharetra metus ac arcu hendrerit, eget fringilla justo tristique. Nullam vulputate ultrices leo sed blandit. Aenean porta malesuada laoreet. Nulla id maximus mi. Nulla sit amet ornare quam, sit amet elementum nulla. Praesent in vestibulum augue, in consectetur erat. Integer at mi luctus, pellentesque elit mattis, malesuada velit. Mauris quis aliquam magna, eget tristique tellus. Aenean a eros leo. Sed egestas efficitur purus non molestie. Donec imperdiet libero vitae vulputate tristique."},{title: "Goals", text: "Ut egestas velit quis lacinia auctor. Praesent id erat semper, lobortis lorem et, sollicitudin augue. Suspendisse sit amet ultrices neque, quis ornare massa. Phasellus nec ligula a lacus egestas facilisis. Donec faucibus mi vitae massa tempus, vitae rutrum enim sodales. Donec sit amet neque ante. Donec porta leo nulla, sed consequat felis auctor nec. Aenean ut urna in nulla vulputate varius. Aliquam a placerat orci, porta tristique purus. Vestibulum volutpat consectetur vulputate."}];
   textExample_2:any = [];
   imageSrc_1 = 'assets/images/support/building.jpg';
-  imageSrc_2 = 'assets/images/support/global-network.jpg'
+  imageSrc_2 = 'assets/images/support/global-network.jpg';
+  paused = true;
+
   constructor(private router: Router, private Route: ActivatedRoute) { 
     this.goodQueary = Route.routeConfig?.path;
     this.title = String(this.goodQueary).toUpperCase();
@@ -22,5 +24,18 @@ export class AboutComponent implements OnInit {
   ngOnInit(): void {
     this.textExample_2 = this.text.split("\n");
   }
+
+  get icon(): string {
+    return this.paused ? `tuiIconPlayLarge` : `tuiIconPauseLarge`;
+  }
+  get styleVideo(): string {
+    return this.paused ? `filter: brightness(0.5)` : `filter: brightness(1)`
+  }
+  get styleBackground(): string {
+    return this.paused ? `opacity: 1` : `opacity: 0`
+  }
+  toggleState(): void {
+    this.paused = !this.paused;
+}
 
 }
